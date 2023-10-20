de87c00ac2da332d455068b1697dd23256ef13a8
Viktor Gamov <gamussa@gmail.com>
2023-10-20 11:07:58 -0400

    small fix

--- routes.go
+++ routes.go
@@ -65,10 +65,11 @@ func SetupRootRoute(router *gin.Engine) {
 		routes := router.Routes()
 
 		var builder strings.Builder
+		builder.WriteString("<h1>Welcome to My httpcan</h1>")
 		builder.WriteString("<h2>List of all endpoints:</h2>")
 		builder.WriteString("<ul>")
 		for _, route := range routes {
-			builder.WriteString(fmt.Sprintf("<li><strong>%s</strong> - <a href=\"%s\">%s</a></li>", route.Method, route.Path, stripLeadingSlash(route.Path)))
+			builder.WriteString(fmt.Sprintf("<li><strong>%s</strong> - <a href=\"%s\">%s</a></li>", route.Method, stripLeadingSlash(route.Path), route.Path))
 		}
 		builder.WriteString("</ul>")
 
